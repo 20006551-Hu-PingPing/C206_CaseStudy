a51bdcaea9f342914e46c83d69ebddd2a495061c
monst <monst@LAPTOP-I68525DJ>
2021-08-05 09:48:15 +0800

    stall class

---
+++ C206_CaseStudy/src/Stall.java
@@ -0,0 +1,20 @@
+
+public class Stall {
+
+	private String name;
+	private String date;
+
+	public Stall(String name, String date) {
+		this.name = name;
+		this.date = date;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+}
